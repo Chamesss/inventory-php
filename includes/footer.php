a9d8800bf6d05f27a9d5aa23fedbcfb8fd0ea2fb
Chamesss <chamsedin.azouz@gmail.com>
2025-04-21 02:47:39 +0100

    environment variables integration

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,12 @@
-<?php?>
+<?php ?>
 
-    </main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> Inventory Management System</p>
-        </div>
-    </footer>
-    <script src="/public/js/script.js"></script>
+</main>
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> Inventory Management System</p>
+    </div>
+</footer>
+<script src="/public/js/script.js"></script>
 </body>
+
 </html>

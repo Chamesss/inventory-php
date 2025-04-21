8732382b80a86825d9fcc5eea2c06c0ea15656b8
Chamesss <chamsedin.azouz@gmail.com>
2025-04-21 02:46:43 +0100

    environment variables integration

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
-require_once realpath('vendor/autoload.php');
+require_once __DIR__ . '/../vendor/autoload.php';
+
 Dotenv\Dotenv::createImmutable(__DIR__ . '/..')->load();
 
 // Database connection configuration
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '7c90e8a7fded2db6dd82ff29084d0e75868786da',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '7c90e8a7fded2db6dd82ff29084d0e75868786da',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
